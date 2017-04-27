39477573896f97a8351330d3d5f235f397a71906
Viv-Iam <vivian.opondoh@gmail.com>
2017-04-28 00:00:02 +0300

    Add comments to root route get() method

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -20,10 +20,13 @@ public class App {
     post("/heros", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
 
+      //fetch user-inputted hero name from the form and save it in String
       String name = request.queryParams("name");
+      //Hero constructor to create new Hero
       Hero newHero = new Hero(name);
+      //Save Hero objects into the user's session
       request.session().attribute("hero", newHero);
-
+      //render success page
       model.put("template", "templates/success.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
