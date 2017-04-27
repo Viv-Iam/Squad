6d745db21d6e6ab0a3368b1c5f75bbb6a1770654
Viv-Iam <vivian.opondoh@gmail.com>
2017-04-27 16:10:33 +0300

    Add first test
    *First test confirms we can successfully instantiate Squad object

--- src/test/java/SquadTest.java
+++ src/test/java/SquadTest.java
@@ -3,5 +3,10 @@ import static org.junit.Assert.*;
 
 public class SquadTest {
 
-  
+//confirm we can successfully instantiate Squad
+  @Test
+  public void Squad_instantiatesCorrectly_true() {
+    Squad mySquad = new Squad("Immortals");
+    assertEquals(true, mySquad instanceof Squad);
+  }
 }
