590011db0891234810fe5e1135d5ec9217daea11
Viv-Iam <vivian.opondoh@gmail.com>
2017-04-27 23:48:42 +0300

    Add hero form
    Add form for users to input hero data in the index.vtl

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -12,7 +12,7 @@ public class App {
 
     get("/", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
-      model.put("task" request.session().attribute())
+      model.put("task" request.session().attribute("task"));
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
