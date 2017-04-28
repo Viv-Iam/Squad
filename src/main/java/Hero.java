b0c103e0a2eadcf08a9a7dfc1a845efa2d47d240
Viv-Iam <vivian.opondoh@gmail.com>
2017-04-28 07:11:52 +0300

    Import Local date and time class in Hero.java and ensure it displays next to Hero name once created

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -1,11 +1,19 @@
+import java.time.LocalDateTime;
+
 public class Hero {
   private String mName;
+  private LocalDateTime mCreatedAt;
 
   public Hero(String name) {
   mName = name;
+  mCreatedAt = LocalDateTime.now();
 
   }
   public String getName() {
     return mName;
   }
+  public LocalDateTime getCreatedAt() {
+    return mCreatedAt;
+  }
+
 }
