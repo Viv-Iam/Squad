ad967ae074d0292a4860dce27a5ce882bc1a8732
Viv-Iam <vivian.opondoh@gmail.com>
2017-04-28 11:35:59 +0300

    Add test
    *Include test ensures we retrive special powers

--- src/test/java/HeroTest.java
+++ src/test/java/HeroTest.java
@@ -8,34 +8,41 @@ public class HeroTest {
 //confirm we can successfully instantiate Squad objects
   @Test
   public void Hero_instantiatesCorrectly_true() {
-    Hero myHero = new Hero("Immortals", 29);
+    Hero myHero = new Hero("Immortals", 29, "sing");
     assertEquals(true, myHero instanceof Hero);
   }
 //ensure we can assign hero a name and retrive it
   @Test
   public void Hero_instantiatesWithName_String() {
-    Hero myHero = new Hero("Immortals", 29);
+    Hero myHero = new Hero("Immortals", 29, "sing");
     assertEquals("Immortals", myHero.getName());
   }
 
   //ensure we can assign hero an age and retrive it
     @Test
     public void Age_instantiatesWithAge_Integer() {
-      Hero myHero = new Hero("Immortal", 29);
+      Hero myHero = new Hero("Immortal", 29, "sing");
       assertEquals(29, myHero.getAge());
+    }
+
+    //ensure we can assign hero a special power and retrive it
+    @Test
+    public void Spower_instantiatesWithAge_String() {
+      Hero myHero = new Hero("Immortal", 29, "sing");
+      assertEquals("sing", myHero.getSpower());
     }
 
   //automatically records date and time hero was entered
   @Test
   public void getCreatedAt_instantiatesWithCurrentTime_today() {
-    Hero myHero = new Hero("Immortals", 29);
+    Hero myHero = new Hero("Immortals", 29, "sing");
     assertEquals(LocalDateTime.now().getDayOfWeek(), myHero.getCreatedAt().getDayOfWeek());
   }
 
   //Retrives all instances of a class
   @Test
   public void all_returnsAllInstancesOfHero_true() {
-    Hero heroOne = new Hero("Mortals", 29);
+    Hero heroOne = new Hero("Mortals", 29, "sing");
     Hero heroTwo = new Hero("Immortals", 29);
     assertEquals(true, Hero.all().contains(heroOne));
     assertEquals(true, Hero.all().contains(heroTwo));
@@ -44,7 +51,7 @@ public class HeroTest {
   //empties the instances ArrayList
   @Test
   public void clear_emptiesAllHerosFromArrayList_0() {
-  Hero myHero = new Hero("Immortals", 29);
+  Hero myHero = new Hero("Immortals", 29, "sing");
   Hero.clear();
   assertEquals(Hero.all().size(), 0);
 }
