c0de07e58aa0edae1d1b3f088be5bd71953bbd7d
Viv-Iam <vivian.opondoh@gmail.com>
2017-04-28 07:21:14 +0300

    Import List and ArrayList classes in Hero.java file

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -1,8 +1,12 @@
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Hero {
   private String mName;
   private LocalDateTime mCreatedAt;
+  private static List<Task> instances = new ArrayList<Task>();
+
 
   public Hero(String name) {
   mName = name;
