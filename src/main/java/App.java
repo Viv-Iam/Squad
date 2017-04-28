7d9b1cbe842bc77c998f9f6c859d500a89547328
Viv-Iam <vivian.opondoh@gmail.com>
2017-04-28 05:30:19 +0300

    Add Array List
    *Add new array List for holding multiple hero sessions and if statment that states if hero sessions do not exist then add option to creat one

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -13,7 +13,7 @@ public class App {
     get("/", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
       //retriving hero data from session and placing it in the model with hero key
-      model.put("hero" request.session().attribute("hero"));
+      model.put("hero", request.session().attribute("hero"));
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
@@ -21,12 +21,18 @@ public class App {
     post("/heros", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
 
+      ArrayList<Hero> heros = request.session().attribute("heros");
+      if (heros == null) {
+        heros = new ArrayList<Hero>();
+        request.session().attribute("tasks", tasks);
+      }
+
       //fetch user-inputted hero name from the form and save it in String
       String name = request.queryParams("name");
       //Hero constructor to create new Hero
       Hero newHero = new Hero(name);
-      //Save Hero objects into the user's session
-      request.session().attribute("hero", newHero);
+      heros.add(newHero);
+
       //render success page
       model.put("template", "templates/success.vtl");
       return new ModelAndView(model, layout);
